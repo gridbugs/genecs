1b02712811d5ceb0cebbeca8c189318b2835184c
Stephen Sherratt <stephen@sherra.tt>
2016-11-12 17:45:59 +1100

    Start of query system

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate tomson;
 extern crate handlebars;
 extern crate rustc_serialize;
 
+use std::collections::HashMap;
 use std::io::Read;
 use std::io::Write;
 use std::path::Path;
@@ -10,12 +11,13 @@ use std::mem;
 
 use tomson::Toml;
 use handlebars::Handlebars;
-use rustc_serialize::json::Json;
+use rustc_serialize::json::{self, Json};
 
 
 const TEMPLATE: &'static str = r#"// Automatically generated. Do not edit.
 use std::collections::{BTreeMap, BTreeSet};
 use std::cell::Cell;
+use std::ptr;
 
 {{#each imports}}
 use {{ this }};
@@ -23,8 +25,8 @@ use {{ this }};
 
 pub type EntityId = u64;
 
-pub type EcsMap<T> = BTreeMap<EntityId, T>;
-pub type EcsSet = BTreeSet<EntityId>;
+pub type EntityMap<T> = BTreeMap<EntityId, T>;
+pub type EntitySet = BTreeSet<EntityId>;
 
 pub const NUM_COMPONENTS: usize = {{num_components}};
 
@@ -36,7 +38,7 @@ const COMPONENT_SET_NUM_WORDS: usize = {{component_set_num_words}};
 pub type ComponentType = usize;
 
 pub mod component_type {
-{{#each components}}
+{{#each component}}
     pub const {{id_uppercase}}: usize = {{index}};
 {{/each}}
 }
@@ -102,7 +104,7 @@ impl ComponentTypeSet {
         ComponentTypeSetIter::new(self.bitfields)
     }
 
-{{#each components}}
+{{#each component}}
     pub fn contains_{{id}}(&self) -> bool {
         self.bitfields[{{set_index}}] & (1 << {{set_bit}}) != 0
     }
@@ -117,22 +119,38 @@ impl ComponentTypeSet {
 {{/each}}
 }
 
+struct DirtyComponentTracker {
+{{#each queried_components}}
+    {{ @key }}: bool,
+{{/each}}
+}
+
+impl DirtyComponentTracker {
+    fn new() -> Self {
+        DirtyComponentTracker {
+{{#each queried_components}}
+            {{ @key }}: true,
+{{/each}}
+        }
+    }
+}
+
 pub struct EcsTable {
-{{#each components}}
-    {{id}}: {{#if type}} EcsMap<{{type}}> {{else}} EcsSet {{/if}},
+{{#each component}}
+    {{id}}: {{#if type}} EntityMap<{{type}}> {{else}} EntitySet {{/if}},
 {{/each}}
 }
 
 impl EcsTable {
     pub fn new() -> Self {
         EcsTable {
-{{#each components}}
-            {{id}}: {{#if type}} EcsMap::new() {{else}} EcsSet::new() {{/if}},
+{{#each component}}
+            {{id}}: {{#if type}} EntityMap::new() {{else}} EntitySet::new() {{/if}},
 {{/each}}
         }
     }
 
-{{#each components}}
+{{#each component}}
     {{#if type}}
     pub fn insert_{{id}}(&mut self, entity: EntityId, value: {{type}}) {
         self.{{id}}.insert(entity, value);
@@ -166,7 +184,7 @@ impl EcsTable {
 
     pub fn remove_component(&mut self, entity: EntityId, component_type: ComponentType) {
         match component_type {
-{{#each components}}
+{{#each component}}
             component_type::{{id_uppercase}} => self.remove_{{id}}(entity),
 {{/each}}
             _ => panic!("Invalid component type: {}", component_type),
@@ -182,24 +200,31 @@ impl EcsTable {
 
 pub struct EcsCtx {
     table: EcsTable,
-    tracker: EcsMap<ComponentTypeSet>,
+    tracker: EntityMap<ComponentTypeSet>,
+    dirty: DirtyComponentTracker,
     next_id: Cell<EntityId>,
+    query_ctx: QueryCtx,
 }
 
 impl EcsCtx {
     pub fn new() -> Self {
         EcsCtx {
             table: EcsTable::new(),
-            tracker: EcsMap::new(),
+            tracker: EntityMap::new(),
+            dirty: DirtyComponentTracker::new(),
             next_id: Cell::new(0),
+            query_ctx: QueryCtx::new(),
         }
     }
 
-{{#each components}}
+{{#each component}}
     {{#if type}}
     pub fn insert_{{id}}(&mut self, entity: EntityId, value: {{type}}) {
         self.table.insert_{{id}}(entity, value);
         self.tracker.entry(entity).or_insert_with(ComponentTypeSet::new).insert_{{id}}();
+        {{#if queried}}
+        self.dirty.{{id}} = true;
+        {{/if}}
     }
 
     pub fn contains_{{id}}(&self, entity: EntityId) -> bool {
@@ -217,6 +242,9 @@ impl EcsCtx {
     pub fn insert_{{id}}(&mut self, entity: EntityId) {
         self.table.insert_{{id}}(entity);
         self.tracker.entry(entity).or_insert_with(ComponentTypeSet::new).insert_{{id}}();
+        {{#if queried}}
+        self.dirty.{{id}} = true;
+        {{/if}}
     }
 
     pub fn contains_{{id}}(&self, entity: EntityId) -> bool {
@@ -233,8 +261,28 @@ impl EcsCtx {
         if let Some(true) = empty {
             self.tracker.remove(&entity);
         }
+        {{#if queried}}
+        self.dirty.{{id}} = true;
+        {{/if}}
     }
 {{/each}}
+
+    pub fn remove_component(&mut self, entity: EntityId, component_type: ComponentType) {
+        match component_type {
+{{#each component}}
+            component_type::{{id_uppercase}} => self.remove_{{id}}(entity),
+{{/each}}
+            _ => panic!("Invalid component type: {}", component_type),
+        }
+    }
+
+
+
+    pub fn remove_components(&mut self, entity: EntityId, component_type_set: ComponentTypeSet) {
+        for component_type in component_type_set.iter() {
+            self.remove_component(entity, component_type);
+        }
+    }
 
     pub fn remove_entity(&mut self, entity: EntityId) {
         if let Some(set) = self.tracker.remove(&entity) {
@@ -299,7 +347,7 @@ impl<'a> EntityRef<'a> {
         }
     }
 
-{{#each components}}
+{{#each component}}
     pub fn contains_{{id}}(self) -> bool {
         self.ctx.contains_{{id}}(self.id)
     }
@@ -339,7 +387,7 @@ impl<'a> EntityRefMut<'a> {
     pub fn destroy(self) {
         self.ctx.remove_entity(self.id);
     }
-{{#each components}}
+{{#each component}}
     pub fn contains_{{id}}(&self) -> bool {
         self.ctx.contains_{{id}}(self.id)
     }
@@ -363,13 +411,89 @@ impl<'a> EntityRefMut<'a> {
     {{/if}}
 {{/each}}
 }
+
+{{#each query}}
+pub struct {{prefix}}Result<'a> {
+    id: EntityId,
+    {{#each components}}
+        {{#if type}}
+    {{id}}: &'a {{type}},
+        {{/if}}
+    {{/each}}
+}
+
+struct {{prefix}}InnerResult {
+    id: EntityId,
+    {{#each components}}
+        {{#if type}}
+    {{id}}: *const {{type}},
+        {{/if}}
+    {{/each}}
+}
+
+impl {{prefix}}InnerResult {
+    fn new_null(id: EntityId) -> Self {
+        {{prefix}}InnerResult {
+            id: id,
+{{#each components}}
+    {{#if type}}
+            {{id}}: ptr::null(),
+    {{/if}}
+{{/each}}
+        }
+    }
+
+    fn to_outer_result(&self) -> {{prefix}}Result {
+        unsafe {
+            {{prefix}}Result {
+                id: self.id,
+{{#each components}}
+    {{#if type}}
+                {{id}}: &(*self.{{id}}),
+    {{/if}}
+{{/each}}
+            }
+        }
+    }
+}
+
+struct {{prefix}}QueryCtx {
+    results: Vec<{{prefix}}InnerResult>,
+}
+
+impl {{prefix}}QueryCtx {
+    fn new() -> Self {
+        {{prefix}}QueryCtx {
+            results: Vec::new(),
+        }
+    }
+}
+{{/each}}
+
+struct QueryCtx {
+{{#each query}}
+    {{id}}: {{prefix}}QueryCtx,
+{{/each}}
+}
+
+impl QueryCtx {
+    fn new() -> Self {
+        QueryCtx {
+{{#each query}}
+            {{id}}: {{prefix}}QueryCtx::new(),
+{{/each}}
+        }
+    }
+}
 "#;
 
 fn generate_code(mut toml: String) -> String {
     // turn the toml string into json for compatibility with handlebars
     let mut json = Toml::as_json(&mut toml).unwrap();
 
-    let num_components = json.search("components").unwrap().as_object().unwrap().len();
+    let mut component_clones = HashMap::new();
+
+    let num_components = json.search("component").unwrap().as_object().unwrap().len();
     json.as_object_mut().unwrap().insert("num_components".to_string(), Json::U64(num_components as u64));
 
     let word_size = mem::size_of::<usize>();
@@ -381,8 +505,19 @@ fn generate_code(mut toml: String) -> String {
     let component_set_num_words = (num_components - 1) / word_bits + 1;
     json.as_object_mut().unwrap().insert("component_set_num_words".to_string(), Json::U64(component_set_num_words as u64));
 
+    let mut queried_components = json::Object::new();
+    for query in json.as_object().unwrap().get("query").unwrap().as_object().unwrap().values() {
+        let query_obj = query.as_object().unwrap();
+        let components_json = query_obj.get("components").unwrap();
+        let components_arr = components_json.as_array().unwrap();
+        for component in components_arr {
+            let component_str = component.as_string().unwrap();
+            queried_components.insert(component_str.to_string(), Json::Boolean(true));
+        }
+    }
+
     let mut index = 0;
-    for (id, component) in json.as_object_mut().unwrap().get_mut("components").unwrap().as_object_mut().unwrap().iter_mut() {
+    for (id, component) in json.as_object_mut().unwrap().get_mut("component").unwrap().as_object_mut().unwrap().iter_mut() {
         let component_obj = component.as_object_mut().unwrap();
         component_obj.insert("index".to_string(), Json::U64(index as u64));
         component_obj.insert("set_index".to_string(), Json::U64((index / word_bits) as u64));
@@ -390,8 +525,30 @@ fn generate_code(mut toml: String) -> String {
         component_obj.insert("id".to_string(), Json::String(id.to_string()));
         component_obj.insert("id_uppercase".to_string(), Json::String(id.to_uppercase()));
 
+        if queried_components.contains_key(id) {
+            component_obj.insert("queried".to_string(), Json::Boolean(true));
+        }
+
+        component_clones.insert(id.to_string(), component_obj.clone());
+
         index += 1;
     }
+
+    for (id, query) in json.as_object_mut().unwrap().get_mut("query").unwrap().as_object_mut().unwrap().iter_mut() {
+        let query_obj = query.as_object_mut().unwrap();
+        query_obj.insert("id".to_string(), Json::String(id.to_string()));
+        let components_json = query_obj.remove("components").unwrap();
+        let components_arr = components_json.as_array().unwrap();
+        let mut components_obj = json::Object::new();
+        for component in components_arr {
+            let component_str = component.as_string().unwrap();
+            let clone = component_clones.get(component_str).unwrap().clone();
+            components_obj.insert(component_str.to_string(), Json::Object(clone));
+        }
+        query_obj.insert("components".to_string(), Json::Object(components_obj));
+    }
+
+    json.as_object_mut().unwrap().insert("queried_components".to_string(), Json::Object(queried_components));
 
     let mut handlebars = Handlebars::new();
 
